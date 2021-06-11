9f686a17b1ffc7c20ccd980473ff9b3c2a18a170
sateesh73 <sateeshvarma73@gmail.com>
2021-06-11 13:16:34 +0530

    spring starter

---
+++ src/test/java/io/online/quize/onlinequize/OnlineQuizeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.online.quize.onlinequize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
